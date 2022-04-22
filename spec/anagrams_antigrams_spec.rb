c368492fd18f609f92aeeff30868ab351b1bd436
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-21 17:06:34 -0700

    passes 1st test for @anagram? method

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -5,7 +5,7 @@ describe(Anagrams)do
   describe('#anagram?')do
     it('Will return true if 2 words are anagrams')do
       word = Anagrams.new()
-      expect(game.anagram?("tea", "eat")).to(eq(true))
+      expect(word.anagram?("tea", "eat")).to(eq(true))
     end
   end
 end
