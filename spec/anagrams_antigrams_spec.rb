9b7e59ec21ef027615fa9ee861321eeaed858029
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-21 17:02:17 -0700

    write first test for @anagram? method

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -1,2 +1,11 @@
 require('rspec')
 require('anagrams_antigrams')
+
+describe(Anagrams)do
+  describe('#anagram?')do
+    it('Will return true if 2 words are anagrams')do
+      word = Anagrams.new()
+      expect(game.anagram?("tea", "eat")).to(eq(true))
+    end
+  end
+end
