434a2a2de17de76910af1b45a27ffcfe4d09d3d7
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-22 13:40:13 -0700

    WIP: refactor tests - remove @anagrams? spec 2 and 3

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -2,22 +2,25 @@ require('rspec')
 require('anagrams_antigrams')
 
 describe(Anagrams)do
-  describe('#anagram?')do
+  describe('#anagrams?')do
     it('Will return true if 2 words are anagrams')do
       word = Anagrams.new()
-      expect(word.anagram?("tea", "eat")).to(eq(true))
+      expect(word.anagrams?("tea", "eat")).to(eq(true))
     end
-
-    it('Will return true if 2 words are anagrams')do
-      word = Anagrams.new()
-      expect(word.anagram?("ruby", "bury")).to(eq(true))
-    end
-
-    # it('Will respond with "These words are anagrams!" if 2 words are anagrams')do
-    #   word = Anagrams.new()
-    #   expect(word.anagram?("tea", "eat")).to(eq("These words are anagrams!"))
-    # end
-    
-
   end
 end
+# describe('#initialize')do
+#   it('Will return words in downcase')do
+#   words = Anagram.new()
+#   expect(word.word1("Hi")).to(eq("hi"))
+#   end
+# end
+# it('Will return lower case if user inputs different cases ')do
+#   word2 = Anagrams.new()
+#   expect(word2.anagrams?("Tea")).to(eq("tea"))
+# end
+
+# it('Will respond with "These words are anagrams!" if 2 words are anagrams')do
+#   word = Anagrams.new()
+#   expect(word.anagram?("tea", "eat")).to(eq("These words are anagrams!"))
+# end
