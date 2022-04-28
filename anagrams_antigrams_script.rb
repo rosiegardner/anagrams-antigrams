f5ce69a40fc1c9e01ccba1e33af5ce70b0dbe2e4
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-27 19:19:07 -0700

    ready to commit

--- anagrams_antigrams_script.rb
+++ anagrams_antigrams_script.rb
@@ -1,2 +1,45 @@
 #!/usr/bin/env ruby
 require './lib/anagrams_antigrams.rb'
+
+puts ""
+puts "   Greetings Earthling!  "
+puts ""
+puts "     .adOOOOOOOOOba. "
+puts "    dOOOOOOOOOOOOOOOb "
+puts "   dOOOOOOOOOOOOOOOOOb "
+puts "  dOOOOOOOOOOOOOOOOOOOb "
+puts " |OOOOOOOOOOOOOOOOOOOOO| "
+puts " OP'~'YOOOOOOOOOOOP'~`YO "
+puts " OO     `YOOOOOP'     OO "
+puts " OOb      `OOO'      dOO "
+puts " YOOo      OOO      oOOP "
+puts " `OOOo     OOO     oOOO' "
+puts "  `OOOb._,dOOOb._,dOOO' "
+puts "   `OOOOOOOOOOOOOOOOO' "
+puts "    OOOOOOOoOoOOOOOOO " 
+puts "    YOOOOOOOOOOOOOOOP " 
+puts "    `OOOOOI```IOOOOO' " 
+puts "     `OOOOI,,,IOOOO' " 
+puts "      `OOOOOOOOOOO' " 
+puts "        `~OOOOO~' " 
+puts ""
+puts "Lets put your knowledge of Anagrams to the test!"
+puts "Are you ready? Because your life depends on it."
+puts "Remember, Anagrams are a word, phrase, or name formed by rearranging the letters of another"
+puts ""
+puts "Enter in your first word or phrase"
+@word1 = gets.chomp.downcase()
+puts "Enter in your second word or phrase"
+@word2 = gets.chomp.downcase()
+words = Anagrams.new(@word1, @word2)
+puts words.anagrams?
+puts words.antigrams?
+puts words.palindrome?
+puts words.words?
+
+
+
+
+
+
+
