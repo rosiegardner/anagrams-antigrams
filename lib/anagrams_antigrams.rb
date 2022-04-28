e4c560a28c0ac254d082d9be987ecb3b737a6f75
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-27 19:18:37 -0700

    ready to commit

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,20 +1,20 @@
+#!/usr/bin/env ruby
 require('pry')
-#ruby-dictionary
-
 
 class Anagrams
   attr_reader :word1, :word2
   def initialize(word1, word2)
-    @word1 = word1.downcase
-    @word2 = word2.downcase
+    @word1 = word1.downcase.delete(' ').gsub(/[^A-Za-z]/, '')
+    @word2 = word2.downcase.delete(' ').gsub(/[^A-Za-z]/, '')
   end
+
   
   def anagrams?() 
     anagrams1 = @word1.split("").sort
     anagrams2 = @word2.split("").sort
     words?
-    if(anagrams1 == anagrams2)
-      "These words are Anagrams!"
+    if (anagrams1 == anagrams2)
+     "These words are Anagrams!"
     end
   end
 
@@ -29,17 +29,22 @@ class Anagrams
   def words?() 
     word = @word1.split(" ")
     word2 = @word2.split(" ")
-    if(word.all? { |word| word.count('aeiou') >= 1 }) && (word2.all? { |word| word2.count('aeiou') >= 1 })
+    if !(word.count('aeiou') >= 1 ) && !(word2.count('aeiou') >= 1 )
       true
-    else
+    else 
       "You need to put in an actual word!"
     end 
   end
+  
+  def palindrome?()
+    palindrome1 = @word1.split("").reverse
+    palindrome2 = @word2.split("").reverse
+    if(palindrome1 == palindrome2)
+      "This anagram is also a palindrome!"
+    end
+  end
+  
 end
 
 
 
-
-
-
-# attr_reader :words
